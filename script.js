9bfdd5fd2e6827a308fe6b6e3424480f7d8e8b4a
VVS <teenwolf23299@gmail.com>
2021-01-02 01:31:11 +0200

    2 rounds for strong ai

--- script.js
+++ script.js
@@ -1,130 +1,207 @@
-const gameBoard=(function(){
-    let board=["","","","","","","","",""];
+const gameBoard = (function () {
+  let board = ["", "", "", "", "", "", "", "", ""];
 
-    //Find DOM's elements
-    let $gridEls=$(".grid-el");
-    
-    
+  //Find DOM's elements
+  let $gridEls = $(".grid-el");
+
+  //events
+  $gridEls.click(addMark);
+
+  //functions
+  function addMark() {
+    if (board[$(this).data("index")] != "") {
+      return;
+    }
+
+    let marker = whoseTurn();
+
+    $(this).text(marker);
+    board[$(this).data("index")] = marker;
+
+    game.checkWin();
+    strongAI.makeMove();
+  }
+  function whoseTurn() {
+    if (game.turn == 1) {
+      game.turn = 2;
+      return game.player1.placeMarker();
+    } else {
+      game.turn = 1;
+      return game.player2.placeMarker();
+    }
+  }
+  function checkBoard() {
+    return board;
+  }
+  return { checkBoard, $gridEls };
+})();
+
+const game = (function () {
+  let turn = 1;
+  let P1score = 0;
+  let P2score = 0;
+  let $score = $(".scores");
+
+  //making players
+  const player = function (marker) {
+    let placeMarker = function () {
+      return marker;
+    };
+    return { placeMarker };
+  };
+  let player1 = player("X");
+  let player2 = player("O");
+
+  function checkWin() {
+    if (
+      (gameBoard.checkBoard()[0] == "X" &&
+        gameBoard.checkBoard()[1] === "X" &&
+        gameBoard.checkBoard()[2] == "X") ||
+      (gameBoard.checkBoard()[3] == "X" &&
+        gameBoard.checkBoard()[4] == "X" &&
+        gameBoard.checkBoard()[5] == "X") ||
+      (gameBoard.checkBoard()[6] == "X" &&
+        gameBoard.checkBoard()[7] == "X" &&
+        gameBoard.checkBoard()[8] == "X") ||
+      (gameBoard.checkBoard()[0] == "X" &&
+        gameBoard.checkBoard()[3] == "X" &&
+        gameBoard.checkBoard()[6] == "X") ||
+      (gameBoard.checkBoard()[1] == "X" &&
+        gameBoard.checkBoard()[4] == "X" &&
+        gameBoard.checkBoard()[7] == "X") ||
+      (gameBoard.checkBoard()[2] == "X" &&
+        gameBoard.checkBoard()[5] == "X" &&
+        gameBoard.checkBoard()[8] == "X") ||
+      (gameBoard.checkBoard()[0] == "X" &&
+        gameBoard.checkBoard()[4] == "X" &&
+        gameBoard.checkBoard()[8] == "X") ||
+      (gameBoard.checkBoard()[2] == "X" &&
+        gameBoard.checkBoard()[4] == "X" &&
+        gameBoard.checkBoard()[6] == "X")
+    ) {
+      changeScore(1);
+    }
+    if (
+      (gameBoard.checkBoard()[0] == "O" &&
+        gameBoard.checkBoard()[1] === "O" &&
+        gameBoard.checkBoard()[2] == "O") ||
+      (gameBoard.checkBoard()[3] == "O" &&
+        gameBoard.checkBoard()[4] == "O" &&
+        gameBoard.checkBoard()[5] == "O") ||
+      (gameBoard.checkBoard()[6] == "O" &&
+        gameBoard.checkBoard()[7] == "O" &&
+        gameBoard.checkBoard()[8] == "O") ||
+      (gameBoard.checkBoard()[0] == "O" &&
+        gameBoard.checkBoard()[3] == "O" &&
+        gameBoard.checkBoard()[6] == "O") ||
+      (gameBoard.checkBoard()[1] == "O" &&
+        gameBoard.checkBoard()[4] == "O" &&
+        gameBoard.checkBoard()[7] == "O") ||
+      (gameBoard.checkBoard()[2] == "O" &&
+        gameBoard.checkBoard()[5] == "O" &&
+        gameBoard.checkBoard()[8] == "O") ||
+      (gameBoard.checkBoard()[0] == "O" &&
+        gameBoard.checkBoard()[4] == "O" &&
+        gameBoard.checkBoard()[8] == "O") ||
+      (gameBoard.checkBoard()[2] == "O" &&
+        gameBoard.checkBoard()[4] == "O" &&
+        gameBoard.checkBoard()[6] == "O")
+    ) {
+      changeScore(2);
+    }
+  }
+  function changeScore(PlayerWin) {
+    if (PlayerWin == 1) {
+      P1score++;
+      $score.find("#P1score").text(P1score);
+    }
+    if (PlayerWin == 2) {
+      P2score++;
+      $score.find("#P2score").text(P2score);
+    }
+    endGame();
+  }
+  function endGame() {
+    gameBoard.$gridEls.off(gameBoard.addMark);
+  }
+
+  return { turn, player1, player2, checkWin };
+})();
 
 
 
-    //events
-    $gridEls.click(addMark)
+const weakAI = (function () {
+  function makeMove() {
+    if (game.turn == 2) {
+      let index;
+      index = Math.floor(Math.random() * 9);
+      if ($("#el" + index).text() == "") {
+        $("#el" + index).click();
+      } else {
+        makeMove();
+      }
+    }
+  }
+  return { makeMove };
+})();
 
-    //functions
-    function addMark(){
-        if (board[$(this).data("index")]!=""){
+
+
+const strongAI = (function () {
+  let counter = 1;  // number of round
+  let board = gameBoard.checkBoard;
+
+  function makeMove() {
+    if (game.turn == 2) {
+      switch (counter) {
+        case 1: //round 1
+          //checking center
+          if (board()[4] == "") {
+            $("#el4").click();
+            counter++;
             return;
-        }
-        
+          } 
+          else {
+            $("#el0").click();
+            counter++;
+            return;
+          }
+          case 2: //round 2
+          for (let i=0;i<8;i++){  //checking for doubles in rows
+              if ([2,5].includes(i)){
+                continue;
+              }
+              if (board()[i]=="X"&&board()[i+1]=="X"){
+                  if([0,3,6].includes(i)){ 
+                    $(`#el${i+2}`).click();
+                    counter++;
+                    return;
+                }
+                else{
+                    $(`#el${i-1}`).click();
+                    counter++;
+                    return;
+                }
+              }
 
-        let marker=whoseTurn();
-        
-
-        $(this).text(marker)
-        board[$(this).data("index")]=marker
-
-        game.checkWin();
-        weakAI.makeMove()
-
+          } // end checking rows
+          for (let i=0;i<3;i++){ //checking columns
+              for (let j=i;j<i+7;j+=3){
+                  if ([6,7,8].includes(j)){
+                      continue;
+                  }
+                  if (board()[j]=="X"&&board()[j+3]=="X"){
+                      if ([0,1,2].includes(j)){
+                        $(`#el${j+6}`).click();
+                      }
+                      else{
+                        $(`#el${j-3}`).click();
+                      }
+                  }
+              }
+          }//stop checking columns 
+      }
     }
-    function whoseTurn(){
-        if (game.turn==1){
-            game.turn=2;
-            return game.player1.placeMarker()
-        }
-        else{
-
-            game.turn=1;
-            return game.player2.placeMarker()
-        }
-
-    }
-    function checkBoard(){
-        return board;
-    }
-    return {checkBoard, $gridEls}
-
-})();
-
-       
-       
-       
-const game=(function(){
-
-    let turn=1;
-    let P1score=0;
-    let P2score=0;
-    let $score=$(".scores");
-
-
-    //making players
-    const player=function(marker){
-   
-        let placeMarker=function(){
-            return marker;
-        }
-        return{placeMarker}
-    
-    }
-    let player1=player("X");
-    let player2=player("O");
-
-
-
-    function checkWin(){
-        if (gameBoard.checkBoard()[0]=="X"&&gameBoard.checkBoard()[1]==="X"&&gameBoard.checkBoard()[2]=="X"||gameBoard.checkBoard()[3]=="X"&&gameBoard.checkBoard()[4]=="X"&&gameBoard.checkBoard()[5]=="X"||gameBoard.checkBoard()[6]=="X"&&gameBoard.checkBoard()[7]=="X"&&gameBoard.checkBoard()[8]=="X"||gameBoard.checkBoard()[0]=="X"&&gameBoard.checkBoard()[3]=="X"&&gameBoard.checkBoard()[6]=="X"||
-        gameBoard.checkBoard()[1]=="X"&&gameBoard.checkBoard()[4]=="X"&&gameBoard.checkBoard()[7]=="X"||gameBoard.checkBoard()[2]=="X"&&gameBoard.checkBoard()[5]=="X"&&gameBoard.checkBoard()[8]=="X"||gameBoard.checkBoard()[0]=="X"&&gameBoard.checkBoard()[4]=="X"&&gameBoard.checkBoard()[8]=="X"||gameBoard.checkBoard()[2]=="X"&&gameBoard.checkBoard()[4]=="X"&&gameBoard.checkBoard()[6]=="X"){
-            changeScore(1);
-        }    
-        if (gameBoard.checkBoard()[0]=="O"&&gameBoard.checkBoard()[1]==="O"&&gameBoard.checkBoard()[2]=="O"||gameBoard.checkBoard()[3]=="O"&&gameBoard.checkBoard()[4]=="O"&&gameBoard.checkBoard()[5]=="O"||gameBoard.checkBoard()[6]=="O"&&gameBoard.checkBoard()[7]=="O"&&gameBoard.checkBoard()[8]=="O"||gameBoard.checkBoard()[0]=="O"&&gameBoard.checkBoard()[3]=="O"&&gameBoard.checkBoard()[6]=="O"||
-        gameBoard.checkBoard()[1]=="O"&&gameBoard.checkBoard()[4]=="O"&&gameBoard.checkBoard()[7]=="O"||gameBoard.checkBoard()[2]=="O"&&gameBoard.checkBoard()[5]=="O"&&gameBoard.checkBoard()[8]=="O"||gameBoard.checkBoard()[0]=="O"&&gameBoard.checkBoard()[4]=="O"&&gameBoard.checkBoard()[8]=="O"||gameBoard.checkBoard()[2]=="O"&&gameBoard.checkBoard()[4]=="O"&&gameBoard.checkBoard()[6]=="O"){
-            changeScore(2);
-        }     
-    }
-    function changeScore(PlayerWin){
-        if(PlayerWin==1){
-            P1score++;
-            $score.find("#P1score").text(P1score);
-
-        }
-        if(PlayerWin==2){
-            P2score++;
-            $score.find("#P2score").text(P2score);
-
-        }
-        endGame();
-    }
-    function endGame(){
-        gameBoard.$gridEls.off(gameBoard.addMark)
-    }
-
-
-return {turn, player1, player2 ,checkWin}
-
-})();
-
-
-
-
-
-
-
-
-const weakAI=(function(){
-    function makeMove(){
-        if(game.turn==2){
-            let index
-            index=Math.floor( Math.random()*9);
-            if ($("#el"+index).text()==""){
-                $("#el"+index).click();
-            }
-             else{
-                 makeMove();
-             }            
-        }
-    }
-    return{makeMove}
-
-
+  }
+  return { makeMove };
 })();
